5b0079b229d13ff53b9fb2442ecb51071c9312ec
SherlockPing <44139918+SherlockPing@users.noreply.github.com>
2020-12-03 21:58:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-$str = file_get_contents("./test.txt", "r") or die("Unable to open file!"); echo base64_encode($str);
+$str = file_get_contents("https://github.com/SherlockPing/v/blob/main/test.txt", "r") or die("Unable to open file!"); echo base64_encode($str);
